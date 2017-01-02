1812df3e186b0f53bb35877c61d0f40ce54c1cf9
Kristin Collins <househuntinginchicago@gmail.com>
2017-01-02 00:29:53 -0600

    Update-checkpoint-23-landing-ls

--- scripts/landing.js
+++ scripts/landing.js
@@ -16,19 +16,21 @@
                 }
     
              };
-
+    //All items that require window to be completely loaded before executing are placed in this block of code
 window.onload = function() {
     
     // animate items automatically on tall screens where scrolling does not initiate animation
     if (window.innerHeight > 950) {
-         animatePoints(pointsArray);
+         animatePoints(pointsArray); 
      }
-    // pulls the items that will be animated
+    
+    // pulls the items that will be animated; in this case, selling-points will be animated.
     var sellingPoints = document.getElementsByClassName('selling-points')[0];
     
-    // set up distance page scrolls before element is viewable in the browser window 
+    // set up distance page scrolls before element is viewable in the browser window.  User must scroll at least 200 //pixels in order for selling points to be seen in the browser. 
     var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
     
+    // when user scrolls something, an event, happens
      window.addEventListener('scroll', function(event) {
          
       if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
