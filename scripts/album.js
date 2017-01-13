351c0fd6f7a7d65188fc98163e3031f2408465b0
Kristin Collins <househuntinginchicago@gmail.com>
2017-01-13 00:00:27 -0600

    checkpoint-26-play/pause-buttons

--- scripts/album.js
+++ scripts/album.js
@@ -52,7 +52,7 @@
  var createSongRow = function(songNumber, songName, songLength) {
      var template =
         '<tr class="album-view-song-item">'
-      + '  <td class="song-item-number">' + songNumber + '</td>'
+      + '  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
       + '  <td class="song-item-title">' + songName + '</td>'
       + '  <td class="song-item-duration">' + songLength + '</td>'
       + '</tr>'
@@ -67,7 +67,7 @@ var albumArtist = document.getElementsByClassName('album-view-artist')[0];
 var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
 var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
 
- var setCurrentAlbum = function(album) { 
+  var setCurrentAlbum = function(album) { 
      // #2
      albumTitle.firstChild.nodeValue = album.title;
      albumArtist.firstChild.nodeValue = album.artist;
@@ -82,10 +82,32 @@ var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
          albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
      }
  };
- 
+      //Elements listeners will be added to
+   var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
+   var songRows = document.getElementsByClassName('album-view-song-item');
+   
+     // Album button templates
+   var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
+
  window.onload = function() {
      setCurrentAlbum(albumPicasso);
- };
+     
+     songListContainer.addEventListener('mouseover', function(event) {
+         
+         // Only target individual song rows during event delegation
+         if (event.target.parentElement.className === 'album-view-song-item') {
+             event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
+         }
+     });
+     
+     for (var i = 0; i < songRows.length; i++) {
+         songRows[i].addEventListener('mouseleave', function(event) {
+             // Selects first child element, which is the song-item-number element
+             this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
+         });
+     }
+
+  };
 
      var count = 0;
      var albumCatalog = [albumPicasso, albumMarconi, albumTaylor];
